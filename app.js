c8a52fe6a7af09aee3c6b988d4661591fd25cbf7
maggieslei <leimaggiese@gmail.com>
2023-10-06 22:16:57 -0400

    add database connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 const PORT = 3000;
 
@@ -14,6 +16,15 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks);
 
-app.listen(PORT, () => {
-  console.log('Server is running in port ' + PORT);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log('Server is running in port ' + PORT);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
