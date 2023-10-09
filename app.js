d9e92c31d3bd35ef683b1c4f7d9f2d1f13d5712e
maggieslei <leimaggiese@gmail.com>
2023-10-08 23:06:13 -0400

    add CRUD functionalities and connect to FE

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const PORT = 3000;
 
+// middleware needed for serving static files
+app.use(express.static('./public'));
 // middleware needed for req.body parsing
 app.use(express.json());
 
